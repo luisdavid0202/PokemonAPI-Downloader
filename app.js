8c5804bfc849accc64ceebe0b2d8262ddcaf5d3c
unknown <luisdavid0202@outlook.com>
2017-09-09 20:25:31 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+var fs = require('fs');
+var sql = require('sql.js');
+var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var filebuffer = fs.readFileSync('assets/data/PokemonDB.db');
+var db = new SQL.Database(filebuffer);
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+
+app.listen(3000, '192.168.1.13', function () {
+    console.log("Corriendo en puerto 3000");
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
+app.get('/', function(req, res, next) {
+// Handle the get for this route
+});
+  
+app.post('/Insert', function(req, res, next) {
+    const parameters = req.body;    
+
+    var id = parameters.id;
+    var name = parameters.name;
+    var weight = parameters.weight;
+    var height = parameters.height;    
+    var baseExp = parameters.base_experience;
+
+    var query = "INSERT INTO Pokemones (Name, Weight, Height, BaseExperience) VALUES ";
+    query += "('"+name+"', "+weight+", "+height+", "+baseExp+");";
+    db.run(query);
+
+    parameters.abilities.forEach(function(item) {
+        var ability = item.ability.name;
+
+        query = "INSERT INTO AbilitiesPokemon (PokemonId, Name) VALUES ";
+        query += "("+id+", '"+ability+"');";
+        db.run(query);         
+    }, this);
+
+    var speed = parameters.stats[0].base_stat;
+    var specialDefense = parameters.stats[1].base_stat;
+    var specialAttack = parameters.stats[2].base_stat;
+    var defense = parameters.stats[3].base_stat;
+    var attack = parameters.stats[4].base_stat;
+    var hp= parameters.stats[5].base_stat ;
+
+    query = "INSERT INTO StatsPokemon (Speed, SpecialDefense, SpecialAttack, Defense, Attack, Hp) VALUES ";
+    query += "("+speed+", "+specialDefense+", "+specialAttack+", "+defense+", "+attack+", "+hp+");";
+    db.run(query);
+
+    var backDefault = parameters.sprites.back_default;
+    var backFemale = parameters.sprites.back_female;
+    var backShiny = parameters.sprites.back_shiny;
+    var backShinyFemale = parameters.sprites.back_shiny_female;
+    var frontDefault = parameters.sprites.front_default;
+    var frontFemale = parameters.sprites.front_female;
+    var frontShiny = parameters.sprites.front_shiny;
+    var frontShinyFemale = parameters.sprites.front_shiny_female;
+
+    query = "INSERT INTO SpritesPokemon (BackDefault, BackFemale, BackShiny, BackShinyFemale, FrontDefault, FrontFemale, FrontShiny, FrontShinyFemale) VALUES ";
+    query += "('"+backDefault+"', '"+backFemale+"', '"+backShiny+"', '"+backShinyFemale+"', '"+frontDefault+"', '"+frontFemale+"', '"+frontShiny+"', '"+frontShinyFemale+"');"
+    db.run(query);
+
+    parameters.types.forEach(function(item){
+        var type = item.type.name;
+
+        query = "INSERT INTO TypesPokemon (PokemonId, Name) VALUES ";
+        query += "("+id+", '"+type+"');";
+        db.run(query);
+    });
+
+    query = "";
+
+    var data = db.export();
+    var buffer = new Buffer(data);
+    fs.writeFileSync("assets/data/pokemonDB.db", buffer);
+
+    console.log(id+" - "+name+ " Created");
+
+    res.status(200).json({message: id+" - "+name+ " Created"});
+});
---
+++ scripts/app.js
@@ -0,0 +1,94 @@
+$(function () {    
+    var jsonObj = [];
+    var id=1;
+    var App = {
+        Init: function () {
+            $('#btnDownloadAll').on('click', function(){
+                App.JSONDownloadAll();
+            });      
+            
+            $('#btnDownloadSingle').on('click', function(){
+                App.JSONDownloadSingle();
+            }); 
+        },
+        ImageDownloader: function (){
+            for(var i = 1; i <= 721; i++){
+                var ruta = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+i+'.png';
+                var enlace = document.createElement('a');
+                enlace.href = ruta;
+                enlace.download = ruta;
+                document.body.appendChild(enlace);
+                enlace.click();                
+                enlace.parentNode.removeChild(enlace);                
+            }
+        },
+        JSONDownloadAll: function(){              
+            for(var i = 1; i <= 721; i++){
+                var _url = "http://pokeapi.co/api/v2/pokemon/"+i;
+
+                $.get({
+                    method: "GET",
+                    url: _url,
+                    type: "JSON",
+                    async: true,
+                    success: function(data){
+                        delete data.forms;
+                        delete data.moves;
+                        delete data.game_indices;                        
+                        jsonObj.push(data);                    
+    
+                        if(jsonObj.length == 721){
+                            jsonObj.sort(function(a, b){
+                                return (a.id - b.id);
+                            });
+
+                            jsonObj.forEach(function(item){
+                                App.Insert(item);
+                            });
+                        }
+                    },
+                    error: function(xhr){
+                        alert("An error occured: " + xhr.status + " " + xhr.statusText);
+                    }
+                });
+            }            
+        },
+        JSONDownloadSingle: function(){
+            var _url = "http://pokeapi.co/api/v2/pokemon/"+id;
+            
+            $.ajax({
+                method: "GET",
+                url: _url,
+                type: "JSON",
+                async: true,
+                success: function(data){
+                    id++;
+                    delete data.forms;
+                    delete data.moves;
+                    delete data.game_indices;
+
+                    App.Insert(data);
+                },
+                error: function(xhr){
+                    alert("An error occured: " + xhr.status + " " + xhr.statusText);
+                }
+            });
+        },
+        Insert: function(_data){
+            $.ajax({
+                method: "POST",
+                url: "http://192.168.1.13:3000/Insert",
+                dataType: "JSON",
+                async: false,
+                data: _data,
+                success: function(res){
+                    console.log(res.message);
+                },
+                error: function(xhr){
+                    alert("An error occured: " + xhr.status + " " + xhr.statusText);
+                }
+            });
+        }
+    };
+    App.Init();
+});
